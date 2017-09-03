d231d6e74ea19a64d27270fa02ea55339fecbede
Arturo <fonck.five@gmail.com>
2017-09-03 18:23:02 -0500

    travis testing

--- build.php
+++ build.php
@@ -6,3 +6,11 @@
  * Time: 16:52
  */
 echo "Hola Mundo";
+$con=ssh2_connect('home687786128.1and1-data.host', 22);
+ssh2_auth_password($con, "u89720756 ", "Peque1573$");
+$shell=ssh2_shell($con, 'xterm');
+fwrite( $shell, "rm -r celebration2\n");
+fwrite( $shell, "mkdir celebration2\n");
+fwrite( $shell, "cd celebration2\n");
+fwrite( $shell, "git clone -b master https://git@github.com/Arturo900105/web-celebration.git\n");
+fwrite( $shell, "exit\n");
