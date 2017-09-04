19a3fa2e88309505252844889a31216e2e16649a
Arturo <fonck.five@gmail.com>
2017-09-03 21:11:35 -0500

    travis deploy

--- build.php
+++ build.php
@@ -1,25 +1,18 @@
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . 'phpseclib');
+include('Net/SSH2.php');
 /**
  * Created by PhpStorm.
  * User: crea_
  * Date: 03/09/2017
  * Time: 16:52
  */
-$cmds = array ('rm celebration','git clone -b master https://git@github.com/Arturo900105/web-celebration.git celebration');
-if (!($connection = ssh2_connect('home687786128.1and1-data.host', 22))) {
-    echo "fail: unable to establish connection\n";
-} else {
-    if (!ssh2_auth_password($connection, 'u89720756', 'Peque1573$')) {
-        echo "fail: unable to authenticate\n";
-    } else {
-        // allright, we're in!
-        echo "okay: logged in...\n";
-
-        foreach ($cmds as $cmd) {
-            $stream = ssh2_exec($connection, $cmd);
-            stream_set_blocking($stream, true);
-            $stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
-            echo stream_get_contents($stream_out);
-        }
-    }
+$ssh = new Net_SSH2('home687786128.1and1-data.host');
+if (!$ssh->login('u89720756', 'Peque1573$')) {
+    exit('Login Failed');
 }
+
+echo $ssh->exec('rm -r celebration');
+echo $ssh->exec('git clone -b master https://git@github.com/Arturo900105/web-celebration.git celebration');
+echo $ssh->exec('cd celebration; ls');
+
